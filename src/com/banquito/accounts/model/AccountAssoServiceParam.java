47dd2a574082c81ff073ebfd49192899eafd4981
Japicado <japicado@espe.edu.ec>
2022-11-20 21:41:48 -0500

    class 23 24 25 added

--- src/com/banquito/accounts/model/AccountAssoServiceParam.java
+++ src/com/banquito/accounts/model/AccountAssoServiceParam.java
@@ -1,3 +1,5 @@
+package com.banquito.accounts.model;
+
 import java.sql.Timestamp;
 
 public class AccountAssoServiceParam {
@@ -8,8 +10,10 @@ public class AccountAssoServiceParam {
     private double numberValue;
     private Timestamp createDate;
     private Timestamp endDate;
+
     public AccountAssoServiceParam() {
     }
+
     public AccountAssoServiceParam(AccountAssoServiceParamPK pk, String status, String textValue, Timestamp dateValuel,
             double numberValue, Timestamp createDate, Timestamp endDate) {
         this.pk = pk;
@@ -20,49 +24,61 @@ public class AccountAssoServiceParam {
         this.createDate = createDate;
         this.endDate = endDate;
     }
+
     public AccountAssoServiceParamPK getPk() {
         return pk;
     }
+
     public void setPk(AccountAssoServiceParamPK pk) {
         this.pk = pk;
     }
+
     public String getStatus() {
         return status;
     }
+
     public void setStatus(String status) {
         this.status = status;
     }
+
     public String getTextValue() {
         return textValue;
     }
+
     public void setTextValue(String textValue) {
         this.textValue = textValue;
     }
+
     public Timestamp getDateValuel() {
         return dateValuel;
     }
+
     public void setDateValuel(Timestamp dateValuel) {
         this.dateValuel = dateValuel;
     }
+
     public double getNumberValue() {
         return numberValue;
     }
+
     public void setNumberValue(double numberValue) {
         this.numberValue = numberValue;
     }
+
     public Timestamp getCreateDate() {
         return createDate;
     }
+
     public void setCreateDate(Timestamp createDate) {
         this.createDate = createDate;
     }
+
     public Timestamp getEndDate() {
         return endDate;
     }
+
     public void setEndDate(Timestamp endDate) {
         this.endDate = endDate;
     }
 
-    
-    
 }
--- src/com/banquito/accounts/model/ProductType.java
+++ src/com/banquito/accounts/model/ProductType.java
@@ -1,3 +1,5 @@
+package com.banquito.accounts.model;
+
 public class ProductType {
 
     private String codeProductType;
@@ -6,8 +8,10 @@ public class ProductType {
     private String allowEarnInterest;
     private String allowGenAccState;
     private String temporalityInterst;
+
     public ProductType() {
     }
+
     public ProductType(String codeProductType, String name, String type, String allowEarnInterest,
             String allowGenAccState, String temporalityInterst) {
         this.codeProductType = codeProductType;
@@ -17,42 +21,53 @@ public class ProductType {
         this.allowGenAccState = allowGenAccState;
         this.temporalityInterst = temporalityInterst;
     }
+
     public String getCodeProductType() {
         return codeProductType;
     }
+
     public void setCodeProductType(String codeProductType) {
         this.codeProductType = codeProductType;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getType() {
         return type;
     }
+
     public void setType(String type) {
         this.type = type;
     }
+
     public String getAllowEarnInterest() {
         return allowEarnInterest;
     }
+
     public void setAllowEarnInterest(String allowEarnInterest) {
         this.allowEarnInterest = allowEarnInterest;
     }
+
     public String getAllowGenAccState() {
         return allowGenAccState;
     }
+
     public void setAllowGenAccState(String allowGenAccState) {
         this.allowGenAccState = allowGenAccState;
     }
+
     public String getTemporalityInterst() {
         return temporalityInterst;
     }
+
     public void setTemporalityInterst(String temporalityInterst) {
         this.temporalityInterst = temporalityInterst;
     }
-    
-    
+
 }
