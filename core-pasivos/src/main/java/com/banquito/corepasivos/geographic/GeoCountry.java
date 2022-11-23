9b358fd54c1bcc422a15bca4e2946badb3f042f7
Jorge Luis Galarza Cruz <44406615+jlgalarza3@users.noreply.github.com>
2022-11-22 22:24:19 -0500

    Add GeoCountry Class (#2)
    * Add GeoCountry Class
    
    * Add onlyExplicitlyIncluded to EqualsAndHashCode

---
+++ core-pasivos/src/main/java/com/banquito/corepasivos/geographic/GeoCountry.java
@@ -0,0 +1,36 @@
+package com.banquito.corepasivos.geographic;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "GEO_COUNTRY")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class GeoCountry {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@Column(name = "CODE_COUNTRY", length = 2, nullable = false)
+	private String codeCountry;
+
+	@Column(name = "CODE_PHONE", length = 3, nullable = false)
+	private String codePhone;
+
+	@Column(name = "NAME", length = 64, nullable = false)
+	private String name;
+
+	public GeoCountry(String codeCountry) {
+		this.codeCountry = codeCountry;
+	}
+
+}
