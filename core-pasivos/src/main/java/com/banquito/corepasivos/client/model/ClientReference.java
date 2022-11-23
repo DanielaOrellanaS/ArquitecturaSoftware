166446c8faf28d01a7baeb61be785490eaa75da5
pacadena5 <pacadena5@espe.edu.ec>
2022-11-23 00:46:41 -0500

    Client reference model

---
+++ core-pasivos/src/main/java/com/banquito/corepasivos/client/model/ClientReference.java
@@ -0,0 +1,33 @@
+package com.banquito.corepasivos.client.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "CLIENT_REFERENCE")
+public class ClientReference {
+    @EqualsAndHashCode.Include
+    @Id
+    @Column(name = "CODE_REFERENCE", length = 8, nullable = false)
+    private String codeReference;
+    @Column(name = "CODE_CLIENT", length = 4, nullable = false)
+    private int codeClient;
+    @Column(name = "NAME", length = 64, nullable = false)
+    private String name;
+    @Column(name = "PHONE", length = 16, nullable = false)
+    private String phone;
+    @Column(name = "RELATED", length = 64, nullable = false)
+    private String related;
+
+    public ClientReference(String codeReference) {
+        this.codeReference = codeReference;
+    }
+}
