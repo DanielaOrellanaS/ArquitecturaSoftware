4b67408a677ea4617aa8cf73c765d4c3f55664c5
listerineh <jsalvarez4@espe.edu.ec>
2022-11-22 23:28:32 -0500

    feat: include InterestRate class

---
+++ core-pasivos/src/main/java/com/banquito/corepasivos/config/model/InterestRate.java
@@ -0,0 +1,36 @@
+package com.banquito.corepasivos.config.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "INTEREST_RATE")
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class InterestRate {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @Column(name = "CODE_INTEREST_RATE", nullable = false, length = 8)
+    private String codeInterestRate;
+
+    @Column(name = "NAME", nullable = false, length = 64)
+    private String name;
+
+    @Column(name = "TYPE", nullable = false, length = 3)
+    private String type;
+
+    @Column(name = "CALC_BASE", nullable = false, length = 8)
+    private String calcBase;
+
+    public InterestRate(String codeInterestRate) {
+        this.codeInterestRate = codeInterestRate;
+    }
+}
