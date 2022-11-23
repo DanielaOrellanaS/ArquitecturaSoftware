5036ae27e39cf73e2be0d7ddfe6ef5ade911e54c
Madely Betancourt <madelybetancourt@gmail.com>
2022-11-23 12:18:41 -0500

    Add JPA entity relationship

--- core-pasivos/src/main/java/com/banquito/corepasivos/geographic/model/GeoLocation.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/geographic/model/GeoLocation.java
@@ -2,7 +2,12 @@ package com.banquito.corepasivos.geographic.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -13,29 +18,36 @@ import lombok.NoArgsConstructor;
 @Table(name = "GEO_LOCATION")
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class GeoLocation {
     
     @EqualsAndHashCode.Include
 	@Id
-	@Column(name = "CODE_LOCATION", length = 5, nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "CODE_LOCATION", nullable = false)
     private Integer codeLocation;
 
-    @Column(name = "GEO_CODE_LOCATION_PARENT", length = 4)
+    @ManyToOne
+    @JoinColumn(name = "GEO_CODE_LOCATION_PARENT", referencedColumnName = "CODE_LOCATION")    
+    @Column(name = "GEO_CODE_LOCATION_PARENT", nullable = true)
     private GeoLocation codeLocationParent;
 
+    @OneToMany(mappedBy = "codeLocationParent")
+    
+    @ManyToOne
     @Column(name = "CODE_COUNTRY", length = 2, nullable = false)
     private GeoCountry codeCountry;
 
     @Column(name = "GEO_LEVEL", length = 2, nullable = false)
     private Integer level;
 
-    @Column(name = "NAME", length = 64)
+    @Column(name = "NAME", length = 64, nullable = true)
     private String name;
 
-    @Column(name = "PHONE_CODE_AREA", length = 2)
+    @Column(name = "PHONE_CODE_AREA", length = 2, nullable = true)
     private String phoneCodeArea;
 
-    @Column(name = "ZIP_CODE", length = 16)
+    @Column(name = "ZIP_CODE", length = 16, nullable = true)
     private String zipCode;
 
     public GeoLocation (Integer codeLocation) {
