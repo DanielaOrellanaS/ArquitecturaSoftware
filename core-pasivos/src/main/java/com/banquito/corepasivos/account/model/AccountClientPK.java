914467dfc727864a888bd68d58a0dfc0d942a409
listerineh <jsalvarez4@espe.edu.ec>
2022-11-23 20:25:24 -0500

    quickfix: accountclientpk

--- core-pasivos/src/main/java/com/banquito/corepasivos/account/model/AccountClientPK.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/account/model/AccountClientPK.java
@@ -5,28 +5,25 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-
 
 @Data
 @Embeddable
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class AccountClientPK implements Serializable{
+public class AccountClientPK implements Serializable {
     @EqualsAndHashCode.Include
-    @Id
-    @Column(name = "CODE_ACCOUNT", nullable= false)
+    @Column(name = "CODE_ACCOUNT", nullable = false)
     private Integer codeAccount;
     @EqualsAndHashCode.Include
-    @Id
-    @Column(name = "CODE_CLIENT", nullable= false)
+    @Column(name = "CODE_CLIENT", nullable = false)
     private Integer codeClient;
-    
-    public AccountClientPK(Integer codeAccount, Integer codeClient){
+
+    public AccountClientPK(Integer codeAccount, Integer codeClient) {
         this.codeAccount = codeAccount;
         this.codeClient = codeClient;
     }
-
 
 }
