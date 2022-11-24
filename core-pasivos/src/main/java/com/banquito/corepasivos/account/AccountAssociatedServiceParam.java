dc07c8d8c2d2f9b072c8648e547ac1c5f2707481
Carlos Alexander Puco Logroño <44406691+carlospuco@users.noreply.github.com>
2022-11-23 19:28:16 -0500

    fix class (#16)

---
+++ core-pasivos/src/main/java/com/banquito/corepasivos/account/AccountAssociatedServiceParam.java
@@ -0,0 +1,45 @@
+package com.banquito.corepasivos.account;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "ACCOUNT_ASSO_SERVICE_PARAM")
+public class AccountAssociatedServiceParam {
+    @EmbeddedId
+    private AccountAssociatedServiceParamPK pk;
+
+    @Column(name = "STATUS", length = 3, nullable = false)
+    private String status;
+    @Column(name = "TEXT_VALUE", length = 128, nullable = false)
+    private String textValue;
+    @Column(name = "DATE_VALUE", nullable = true)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Timestamp dateValue;
+    @Column(name = "NUMBER_VALUE", length = 17, precision = 2, nullable = true)
+    private Number numberValue;
+    @Column(name = "CREATE_DATE", nullable = true)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Timestamp createDate;
+    @Column(name = "END_DATE", nullable = true)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Timestamp endDate;
+
+    public AccountAssociatedServiceParam(AccountAssociatedServiceParamPK pk) {
+        this.pk = pk;
+    }
+
+}
---
+++ core-pasivos/src/main/java/com/banquito/corepasivos/product/model/ProductType.java
@@ -0,0 +1,37 @@
+package com.banquito.corepasivos.product.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "PRODUCT_TYPE")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class ProductType {
+    @EqualsAndHashCode.Include
+    @Id
+    @Column(name = "CODE_PRODUCT_TYPE", length = 32, nullable = false)
+    private String codeProductType;
+    @Column(name = "NAME", length = 64, nullable = false)
+    private String name;
+    @Column(name = "TYPE", length = 3, nullable = false)
+    private String type;
+    @Column(name = "ALLOW_EARN_INTEREST", length = 1, nullable = false)
+    private String allowEarnInterest;
+    @Column(name = "ALLOW_GEN_ACC_STATE", length = 1, nullable = false)
+    private String allowGeneralAccountState;
+    @Column(name = "TEMPORALITY_INTEREST", length = 3, nullable = true )
+    private String temporalityInterest;
+
+    public ProductType(String codeProductType){
+        this.codeProductType=codeProductType;
+    }
+
+}
