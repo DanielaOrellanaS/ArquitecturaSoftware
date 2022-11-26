53a6dedf2dd2090d8ab73983fe01f617fe222692
Damoz1606 <panchitodmun@gmail.com>
2022-11-25 21:52:04 -0500

    Segment and ClientReference repositories

---
+++ core-pasivos/src/main/java/com/banquito/corepasivos/client/repository/ClientReferenceRepository.java
@@ -0,0 +1,9 @@
+package com.banquito.corepasivos.client.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.banquito.corepasivos.client.model.ClientReference;
+
+public interface ClientReferenceRepository extends JpaRepository<ClientReference, Integer> {
+
+}
