7e6a0c70dde1f8097b8b315f95fa22dbd36babc3
Damoz1606 <panchitodmun@gmail.com>
2022-11-25 22:46:56 -0500

    Remove unused references

--- core-pasivos/src/main/java/com/banquito/corepasivos/client/model/ClientReference.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/client/model/ClientReference.java
@@ -1,13 +1,8 @@
 package com.banquito.corepasivos.client.model;
 
 import javax.persistence.Column;
-import javax.persistence.Embedded;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinColumns;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
 import javax.persistence.Table;
