a7494c18c2e98760c2bedcd5045ed62bcab3aff3
AlexGermancw <agchicaiza2@espe.edu.ec>
2022-11-28 11:24:21 -0500

    Correcion General -Location, Structure, PK

--- core-pasivos/src/main/java/com/banquito/corepasivos/general/model/LocationEntity.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/general/model/LocationEntity.java
@@ -1,15 +1,12 @@
 package com.banquito.corepasivos.general.model;
 
 import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinColumns;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -19,19 +16,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class LocationEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "code_location", nullable = false)
-    private Integer codeLocation;
-
-    @Column(name = "code_location_parent", nullable = false)
-    private Integer codeLocationParent;
-
-    @Column(name = "level", nullable = false)
-    private Integer level;
-
-    @Column(name = "code_country", length = 2, nullable = false)
-    private String codeCountry;
+    @EmbeddedId
+    private LocationEntityPK pk;
 
     @Column(name = "name", length = 64, nullable = false)
     private String name;
@@ -43,21 +29,14 @@ public class LocationEntity {
     private String zipCode;
 
     @ManyToOne
-    @JoinColumn(name = "code_location_parent", referencedColumnName = "code_location", insertable = false, updatable = false, nullable = true)
-    private LocationEntity locationEntity;
+    @JoinColumns({
+        @JoinColumn(name = "code_country", referencedColumnName = "code_country", insertable = false, updatable = false, nullable = true),
+    })
+    private CountryEntity countryEntity;
 
     @ManyToOne
     @JoinColumns({
-        @JoinColumn(name = "level", referencedColumnName = "geo_level", insertable = false, updatable = false, nullable = true),
-        @JoinColumn(name = "code_country", referencedColumnName = "code_country", insertable = false, updatable = false, nullable = true)
+        @JoinColumn(name = "geo_level", referencedColumnName = "geo_level", insertable = false, updatable = false, nullable = true),
     })
     private StructureEntity structureEntity;
-
-    @ManyToOne
-    @JoinColumn(name = "code_country", referencedColumnName = "code_country", insertable = false, updatable = false, nullable = true)
-    private CountryEntity countryEntity;
-
-    public LocationEntity(Integer codeLocation) {
-        this.codeLocation = codeLocation;
-    }
 }
--- core-pasivos/src/main/java/com/banquito/corepasivos/general/model/StructureEntity.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/general/model/StructureEntity.java
@@ -3,8 +3,6 @@ package com.banquito.corepasivos.general.model;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -21,10 +19,6 @@ public class StructureEntity {
 
     @Column(name = "name", length = 64, nullable = false)
     private String name;
-
-    @ManyToOne
-    @JoinColumn(name = "code_country", referencedColumnName = "code_country", insertable = false, updatable = false, nullable = true)
-    private CountryEntity countryEntity;
 
     public StructureEntity(StructureEntityPK pk) {
         this.pk = pk;
