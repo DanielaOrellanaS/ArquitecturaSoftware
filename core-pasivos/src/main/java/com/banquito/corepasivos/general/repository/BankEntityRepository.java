790f0846e58050c3ffe639a499e6f4db97af03e6
Japicado <japicado@espe.edu.ec>
2022-11-28 10:29:02 -0500

    Model, controller, repo and service files from BankEntity and CountryEntity class added

--- core-pasivos/src/main/java/com/banquito/corepasivos/general/repository/BankEntityRepository.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/general/repository/BankEntityRepository.java
@@ -1,15 +1,9 @@
 package com.banquito.corepasivos.general.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.banquito.corepasivos.general.model.BankEntity;
 
 public interface BankEntityRepository extends JpaRepository<BankEntity, String> {
-
-    List<BankEntity> findByEntityBankCode(String entityBankCode);
-
-    List<BankEntity> findByInternationalBankCode(String internationalBankCode);
 
 }
