07b1a73376f68560a9900aa78ce618d4bac23a0d
Japicado <japicado@espe.edu.ec>
2022-11-28 10:35:52 -0500

    fixed colum name

--- core-pasivos/src/main/java/com/banquito/corepasivos/general/repository/BankEntityRepository.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/general/repository/BankEntityRepository.java
@@ -3,7 +3,8 @@ package com.banquito.corepasivos.general.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.banquito.corepasivos.general.model.BankEntity;
+import com.banquito.corepasivos.general.model.BankEntityPK;
 
-public interface BankEntityRepository extends JpaRepository<BankEntity, String> {
+public interface BankEntityRepository extends JpaRepository<BankEntity, BankEntityPK> {
 
 }
