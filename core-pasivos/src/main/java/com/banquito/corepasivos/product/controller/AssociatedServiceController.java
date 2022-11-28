14a0ceb13ceb86bfdf45095c466fd1b1da1f972d
listerineh <jsalvarez4@espe.edu.ec>
2022-11-28 09:37:55 -0500

    add: associated service controller

---
+++ core-pasivos/src/main/java/com/banquito/corepasivos/product/controller/AssociatedServiceController.java
@@ -0,0 +1,21 @@
+package com.banquito.corepasivos.product.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.banquito.corepasivos.product.service.AssociatedServiceService;
+
+@RestController
+@RequestMapping("/associatedservice")
+public class AssociatedServiceController {
+
+    @Autowired
+    AssociatedServiceService associatedServiceService;
+
+    @GetMapping("/all")
+    public Object getAllAssociatedServices() {
+        return this.associatedServiceService.getAllAssociatedServices();
+    }
+}
