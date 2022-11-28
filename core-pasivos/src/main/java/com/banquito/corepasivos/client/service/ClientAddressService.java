991fb612681f388574ef4dfa4a6b2d2155f4abcb
BRYAN ALEXANDER JIMENEZ DELGADO <45211470+bryanjd007@users.noreply.github.com>
2022-11-28 10:40:42 -0500

    Create Service ClientAddress

---
+++ core-pasivos/src/main/java/com/banquito/corepasivos/client/service/ClientAddressService.java
@@ -0,0 +1,9 @@
+package com.banquito.corepasivos.client.service;
+
+import java.util.List;
+
+import com.banquito.corepasivos.client.model.ClientAddress;
+
+public interface ClientAddressService {
+    List<ClientAddress> getAllClientAddress();
+}
