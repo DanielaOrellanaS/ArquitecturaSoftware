21a437ba37f1c6941ec1291b8d92d2c54289cc9b
Damoz1606 <panchitodmun@gmail.com>
2022-11-28 10:19:41 -0500

    Set annotation

--- core-pasivos/src/main/java/com/banquito/corepasivos/client/repository/ClientReferenceRepository.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/client/repository/ClientReferenceRepository.java
@@ -3,11 +3,16 @@ package com.banquito.corepasivos.client.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.banquito.corepasivos.client.model.ClientReference;
+import com.banquito.corepasivos.client.model.ClientReferencePK;
 
-public interface ClientReferenceRepository extends JpaRepository<ClientReference, Integer> {
+@Repository
+public interface ClientReferenceRepository extends JpaRepository<ClientReference, ClientReferencePK> {
     List<ClientReference> findByPkCode(Integer codeReference);
+
     List<ClientReference> findByPkIdentificationtype(String identificationType);
+
     List<ClientReference> findByPkIdentification(String identification);
 }
