a8aff012bac74b3fbae6cf2687a8abbcdcfc4a19
Carvajal Patiño Luis Enrique <44406616+carvajalluisweb@users.noreply.github.com>
2022-12-05 17:21:56 -0500

    Transaction services

--- core-pasivos/src/main/java/com/banquito/corepasivos/account/services/AccountTransactionService.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/account/services/AccountTransactionService.java
@@ -4,6 +4,7 @@ import com.banquito.corepasivos.account.model.AccountTransaction;
 import com.banquito.corepasivos.account.repository.AccountTransactionRepository;
 import org.springframework.stereotype.Service;
 
+
 import java.util.List;
 
 @Service
@@ -18,4 +19,55 @@ public class AccountTransactionService {
     public List<AccountTransaction> findAll(){
         return this.accountTransactionRepository.findAll();
     }
+
+    public List<AccountTransaction> findByCodeLocalAccount(String codeLocalAccount) {
+		List<AccountTransaction> accountTransaction = this.accountTransactionRepository
+				.findByCodeLocalAccount(codeLocalAccount);
+		if (accountTransaction.isEmpty()) {
+			throw new RuntimeException("Account Transaction by Account not found");
+		} else {
+			return accountTransaction;
+		}
+	}
+	
+	public List<AccountTransaction> findByCodeUniqueTransaction(String codeUniqueTransaction) {
+		List<AccountTransaction> accountTransaction = this.accountTransactionRepository
+				.findByCodeUniqueTransaction(codeUniqueTransaction);
+		if (accountTransaction.isEmpty()) {
+			throw new RuntimeException("Account Transaction not found");
+		} else {
+			return accountTransaction;
+		}
+	}
+
+	public List<AccountTransaction> findByStatus(String status) {
+		List<AccountTransaction> accountTransaction = this.accountTransactionRepository
+				.findByStatus(status);
+		if (accountTransaction.isEmpty()) {
+			throw new RuntimeException("Account not found");
+		} else {
+			return accountTransaction;
+		}
+	}
+
+
+	public List<AccountTransaction> findByRecipientBank(String recipientBank) {
+		List<AccountTransaction> accountTransaction = this.accountTransactionRepository
+				.findByRecipientBank(recipientBank);
+		if (accountTransaction.isEmpty()) {
+			throw new RuntimeException("Account Transaction by Recipient Bank not found");
+		} else {
+			return accountTransaction;
+		}
+	}
+
+	// public List<AccountTransaction> findByDate(Date start, Date end) {
+	// 	List<AccountTransaction> accountTransaction = this.accountTransactionRepository
+	// 			.findByDate(start, end);
+	// 	if (accountTransaction.isEmpty()) {
+	// 		throw new RuntimeException("Account Transaction by Date not found");
+	// 	} else {
+	// 		return accountTransaction;
+	// 	}
+	// }
 }
