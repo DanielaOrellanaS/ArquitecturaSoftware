5defb171c35b78e3ea7b171b48dd58381b8d1682
Kevinhurtado <44406663+Kevinhurtado@users.noreply.github.com>
2022-12-05 16:55:46 -0500

    Add methods to create, update and delete

--- core-pasivos/src/main/java/com/banquito/corepasivos/product/controller/ProductController.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/product/controller/ProductController.java
@@ -2,18 +2,23 @@ package com.banquito.corepasivos.product.controller;
 
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.banquito.corepasivos.product.model.Product;
+import com.banquito.corepasivos.product.model.ProductPK;
 import com.banquito.corepasivos.product.service.ProductService;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/api/products")
 public class ProductController {
 
     private final ProductService productService;
@@ -46,9 +51,40 @@ public class ProductController {
         return product == null ? ResponseEntity.noContent().build() : ResponseEntity.ok(product);
     }
 
-    @PostMapping
-    public void createProduct(Product product) {
-        productService.createProduct(product);
+    @PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<String> createProduct(@RequestBody Product product) {
+        try {
+            this.productService.createProduct(product);
+            return ResponseEntity.ok("Product created successfully");
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    @PutMapping(consumes = { "application/json" })
+    public ResponseEntity<String> updateProduct(
+            @RequestBody Product product) {
+        try {
+            this.productService.updateProduct(product);
+            return ResponseEntity.ok("Product updated successfully");
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/code/{codeProduct}/code/type/{codeProductType}")
+    public ResponseEntity<String> deleteProduct(
+            @PathVariable("codeProduct") String codeProduct,
+            @PathVariable("codeProductType") String codeProductType) {
+        try {
+            ProductPK pk = new ProductPK();
+            pk.setCodeProduct(codeProduct);
+            pk.setCodeProductType(codeProductType);
+            this.productService.deleteProduct(pk);
+            return ResponseEntity.ok("Product removed successfully");
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
     }
     
 }
