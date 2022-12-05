d02f05136d148d01d0a2c00dfadd14cbae4a553a
Carvajal Patiño Luis Enrique <44406616+carvajalluisweb@users.noreply.github.com>
2022-12-05 18:37:48 -0500

    Fix api name

--- core-pasivos/src/main/java/com/banquito/corepasivos/account/controller/AccountTransactionController.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/account/controller/AccountTransactionController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/account-transaction")
+@RequestMapping("api/account-transaction")
 public class AccountTransactionController {
 
     private final AccountTransactionService accountTransactionService;
