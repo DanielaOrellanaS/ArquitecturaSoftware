9b42d2789a6c1a9b8ed4ec8bcd7d854ff8b8b58a
Diego Zurita <diegoalexandermh@gmail.com>
2022-12-04 20:32:15 -0500

    refactor repository

--- core-pasivos/src/main/java/com/banquito/corepasivos/product/repository/InterestRateLogRepository.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/product/repository/InterestRateLogRepository.java
@@ -3,9 +3,11 @@ package com.banquito.corepasivos.product.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.banquito.corepasivos.product.model.InterestRateLog;
 
+@Repository
 public interface InterestRateLogRepository extends JpaRepository<InterestRateLog, String>{
     List<InterestRateLog> findByCodeInterestRateLog(String codeInterestRateLog);
     
--- core-pasivos/src/main/java/com/banquito/corepasivos/product/repository/InterestRateRepository.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/product/repository/InterestRateRepository.java
@@ -3,9 +3,11 @@ package com.banquito.corepasivos.product.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.banquito.corepasivos.product.model.InterestRate;
 
+@Repository
 public interface InterestRateRepository extends JpaRepository<InterestRate, String> {
     List<InterestRate> findByCodeInterestRate(String codeInterestRate);
     
