3bc005feee01d7a852d4a63b30e1537d6e5c4166
Kevinhurtado <44406663+Kevinhurtado@users.noreply.github.com>
2022-12-06 15:01:11 -0500

    Add a new method in ProductRepository to verify if a product exists

--- core-pasivos/src/main/java/com/banquito/corepasivos/product/repository/ProductRepository.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/product/repository/ProductRepository.java
@@ -19,4 +19,6 @@ public interface ProductRepository extends JpaRepository<Product, ProductPK>{
 
     List<Product> findByName(String name);
 
+    Boolean existsByPkCodeProduct(String codeProduct);
+
 }
