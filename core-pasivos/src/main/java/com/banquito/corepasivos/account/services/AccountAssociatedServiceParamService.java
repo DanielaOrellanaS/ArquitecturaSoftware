900cf2f2378c6424ac4aaa53d0f585ac099bf618
Jorge Galarza <jorge2000cruz@gmail.com>
2022-12-05 20:21:57 -0500

    Adding AccountAssociatedServiceParam Services: CRUD

--- core-pasivos/src/main/java/com/banquito/corepasivos/account/services/AccountAssociatedServiceParamService.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/account/services/AccountAssociatedServiceParamService.java
@@ -6,15 +6,117 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 @Service
 public class AccountAssociatedServiceParamService {
-    private final AccountAssociatedServiceParamRepository accountAssociatedServiceParamRepository;
+	private final AccountAssociatedServiceParamRepository accountAssociatedServiceParamRepository;
 
-    public AccountAssociatedServiceParamService(AccountAssociatedServiceParamRepository accountAssociatedServiceParamRepository) {
-        this.accountAssociatedServiceParamRepository = accountAssociatedServiceParamRepository;
-    }
+	public AccountAssociatedServiceParamService(
+			AccountAssociatedServiceParamRepository accountAssociatedServiceParamRepository) {
+		this.accountAssociatedServiceParamRepository = accountAssociatedServiceParamRepository;
+	}
 
-    public List<AccountAssociatedServiceParam> findAll(){
-        return accountAssociatedServiceParamRepository.findAll();
-    }
+	@Transactional
+	public List<AccountAssociatedServiceParam> findByCodeLocalAccount(String codeLocalAccount) {
+		List<AccountAssociatedServiceParam> accountAssociatedServiceParamList = this.accountAssociatedServiceParamRepository
+				.findByCodeLocalAccount(codeLocalAccount);
+		if (accountAssociatedServiceParamList.isEmpty()) {
+			throw new RuntimeException("Account Associated Service Param not found");
+		} else {
+			return accountAssociatedServiceParamList;
+		}
+	}
+
+	@Transactional
+	public List<AccountAssociatedServiceParam> findByCodeInternationalAccount(String codeInternationalAccount) {
+		List<AccountAssociatedServiceParam> accountAssociatedServiceParamList = this.accountAssociatedServiceParamRepository
+				.findByCodeInternationalAccount(codeInternationalAccount);
+		if (accountAssociatedServiceParamList.isEmpty()) {
+			throw new RuntimeException("Account Associated Service Param not found");
+		} else {
+			return accountAssociatedServiceParamList;
+		}
+	}
+
+	@Transactional
+	public List<AccountAssociatedServiceParam> findByCodeParam(String codeParam) {
+		List<AccountAssociatedServiceParam> accountAssociatedServiceParamList = this.accountAssociatedServiceParamRepository
+				.findByCodeParam(codeParam);
+		if (accountAssociatedServiceParamList.isEmpty()) {
+			throw new RuntimeException("Account Associated Service Param not found");
+		} else {
+			return accountAssociatedServiceParamList;
+		}
+	}
+
+	@Transactional
+	public void save(AccountAssociatedServiceParam accountAssociatedServiceParam) {
+		try {
+			this.accountAssociatedServiceParamRepository.save(accountAssociatedServiceParam);
+		} catch (Exception e) {
+			throw new RuntimeException("Error saving Account Associated Service Param");
+		}
+	}
+
+	@Transactional
+	public void update(AccountAssociatedServiceParam accountAssociatedServiceParam) {
+		try {
+			this.accountAssociatedServiceParamRepository.save(accountAssociatedServiceParam);
+		} catch (Exception e) {
+			throw new RuntimeException("Error updating Account Associated Service Param");
+		}
+	}
+
+	@Transactional
+	public void deleteAllByCodeLocalAccount(String codeLocalAccount) {
+
+		List<AccountAssociatedServiceParam> accountAssociatedServiceParamList = this.accountAssociatedServiceParamRepository
+				.findByCodeLocalAccount(codeLocalAccount);
+
+		if (accountAssociatedServiceParamList.isEmpty()) {
+			throw new RuntimeException("Account Associated Service Param not found");
+		} else {
+			for (AccountAssociatedServiceParam accountAssociatedServiceParam : accountAssociatedServiceParamList) {
+				accountAssociatedServiceParam.setStatus("INA");
+				this.accountAssociatedServiceParamRepository.save(accountAssociatedServiceParam);
+			}
+		}
+	}
+
+	@Transactional
+	public void deleteAllByCodeInternationalAccount(String codeInternationalAccount) {
+
+		List<AccountAssociatedServiceParam> accountAssociatedServiceParamList = this.accountAssociatedServiceParamRepository
+				.findByCodeInternationalAccount(codeInternationalAccount);
+
+		if (accountAssociatedServiceParamList.isEmpty()) {
+			throw new RuntimeException("Account Associated Service Param not found");
+		} else {
+			for (AccountAssociatedServiceParam accountAssociatedServiceParam : accountAssociatedServiceParamList) {
+				accountAssociatedServiceParam.setStatus("INA");
+				this.accountAssociatedServiceParamRepository.save(accountAssociatedServiceParam);
+			}
+		}
+	}
+
+	@Transactional
+	public void deleteAllByCodeParam(String codeParam) {
+
+		List<AccountAssociatedServiceParam> accountAssociatedServiceParamList = this.accountAssociatedServiceParamRepository
+				.findByCodeParam(codeParam);
+
+		if (accountAssociatedServiceParamList.isEmpty()) {
+			throw new RuntimeException("Account Associated Service Param not found");
+		} else {
+			for (AccountAssociatedServiceParam accountAssociatedServiceParam : accountAssociatedServiceParamList) {
+				accountAssociatedServiceParam.setStatus("INA");
+				this.accountAssociatedServiceParamRepository.save(accountAssociatedServiceParam);
+			}
+		}
+	}
+
+	public List<AccountAssociatedServiceParam> findAll() {
+		return accountAssociatedServiceParamRepository.findAll();
+	}
 }
