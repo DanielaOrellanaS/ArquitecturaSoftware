8102ac8513658cc380d50fc5bf29a024b02e11ea
Abigail Cabascango <sacabascango1espe.edu.ec>
2022-12-07 00:42:56 -0500

    feat: Catalog of services rest with swagger

--- core-pasivos/src/main/java/com/banquito/corepasivos/config/SwaggerConfig.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/config/SwaggerConfig.java
@@ -1,11 +1,25 @@
 package com.banquito.corepasivos.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import io.micrometer.core.ipc.http.HttpSender.Request;
+import com.wordnik.swagger.models.Contact;
+
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,12 +27,34 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    @Primary
     @Bean
-    public Docket api(){
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+
+    }
+    @Bean
+    public Docket postsApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-            .select()
-            .apis(RequestHandlerSelectors.basePackage("com.banquito.corepasivos"))
-            .paths(PathSelectors.any())
-            .build();
+                .groupName("")
+                .apiInfo(buildApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("src/main/java/com/banquito/corepasivos/client/controller/.*"))
+                .build();
+    }
+
+    private ApiInfo buildApiInfo() {
+        Contact contact = new Contact();
+        return new ApiInfoBuilder()
+                .title("CorePasivosApplication")
+                .description("API Description")
+                .license("license")
+                .version("1.0")
+                .contact(contact.toString())
+                .licenseUrl("licenseURl")
+                .build();
     }
 }
