00144ba709a607514291a7ea4bbf4017efd3bdf0
Daniela Estefania Orellana Soriano <63482841+DanielaOrellanaS@users.noreply.github.com>
2022-12-06 22:38:12 -0500

    Fix StructureEntityRepository

--- core-pasivos/src/main/java/com/banquito/corepasivos/general/repository/StructureEntityRepository.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/general/repository/StructureEntityRepository.java
@@ -1,7 +1,5 @@
 package com.banquito.corepasivos.general.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,8 +7,6 @@ import com.banquito.corepasivos.general.model.StructureEntity;
 import com.banquito.corepasivos.general.model.StructureEntityPK;
 
 @Repository
-public interface StructureEntityReposiroty extends JpaRepository<StructureEntity, StructureEntityPK> {
-    List<StructureEntity> findByCodeCountry(String internationalBankCode);
+public interface StructureEntityRepository extends JpaRepository<StructureEntity, StructureEntityPK> {
 
-    List<StructureEntity> findAll();
 }
