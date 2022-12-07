610d024cf381d5cf474eab769196e91cc09829e2
Daniela Estefania Orellana Soriano <63482841+DanielaOrellanaS@users.noreply.github.com>
2022-12-06 22:36:27 -0500

    Fix BankEntityRepository

--- core-pasivos/src/main/java/com/banquito/corepasivos/general/repository/BankEntityRepository.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/general/repository/BankEntityRepository.java
@@ -1,7 +1,5 @@
 package com.banquito.corepasivos.general.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,13 +8,5 @@ import com.banquito.corepasivos.general.model.BankEntityPK;
 
 @Repository
 public interface BankEntityRepository extends JpaRepository<BankEntity, BankEntityPK> {
-
-    List<BankEntity> findByPkInternationalBankCode(String internationalBankCode);
-
-    List<BankEntity> findByPkEntityBankCode(String entityBankCode);
-
-    List<BankEntity> findByName(String name);
-
-    List<BankEntity> findAll();
 
 }
