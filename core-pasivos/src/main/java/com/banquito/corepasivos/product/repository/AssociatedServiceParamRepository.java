880b700fd654b6dca67c6b397f8bcddc2e8bf7cf
Damoz1606 <panchitodmun@gmail.com>
2022-12-12 14:51:47 -0500

    Add repository tag on AssociatedServiceParamRepository

--- core-pasivos/src/main/java/com/banquito/corepasivos/product/repository/AssociatedServiceParamRepository.java
+++ core-pasivos/src/main/java/com/banquito/corepasivos/product/repository/AssociatedServiceParamRepository.java
@@ -3,10 +3,12 @@ package com.banquito.corepasivos.product.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.banquito.corepasivos.product.model.AssociatedServiceParam;
 import com.banquito.corepasivos.product.model.AssociatedServiceParamPK;
 
+@Repository
 public interface AssociatedServiceParamRepository
         extends JpaRepository<AssociatedServiceParam, AssociatedServiceParamPK> {
     List<AssociatedServiceParam> findByPkCodeParam(String codeParam);
